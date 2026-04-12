a886c9beb152032965fc661ae2b1dc3e7b4a6770
elementkz <adamroitub7@gmail.com>
2026-04-12 15:10:11 +0300

    added description expand state

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 function JobListing({ job }) {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "...";
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -7,8 +17,13 @@ function JobListing({ job }) {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
-
+        <div className="mb-5">{description}</div>
+        <button
+          onClick={() => setShowFullDescription((prev) => !prev)}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          Show {showFullDescription ? "less" : "more"}
+        </button>
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
         <div className="border border-gray-100 mb-5"></div>
@@ -20,7 +35,7 @@ function JobListing({ job }) {
           </div>
           <a
             href={`/job/${job.id}`}
-            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+            className="h-9 bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
           </a>
